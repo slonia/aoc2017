385842fb1abd3c1178dd5940a1b1b2006e36909b
Ilya Kolodnik <ilya.kolodnik@gmail.com>
2017-12-25 18:56:50 +0300

    day 5/1

---
+++ day05/instructions.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+func main() {
+	file, _ := os.Open("input.txt")
+	scanner := bufio.NewScanner(file)
+	scanner.Split(bufio.ScanLines)
+	numbers := []int{}
+	for scanner.Scan() {
+		line := scanner.Text()
+		number, _ := strconv.Atoi(line)
+		numbers = append(numbers, number)
+	}
+	ind := 0
+	steps := 0
+	for ind < len(numbers) {
+		// print(numbers, ind)
+		new_ind := ind + numbers[ind]
+		numbers[ind]++
+		ind = new_ind
+		steps++
+	}
+	fmt.Printf("%v\n", steps)
+}
+
+func print(numbers []int, ind int) {
+	fmt.Printf("%v - ", ind)
+	for i, el := range numbers {
+		if i == ind {
+			fmt.Printf("[%v] ", el)
+		} else {
+			fmt.Printf("%v ", el)
+		}
+	}
+	fmt.Printf("\n")
+
+}
