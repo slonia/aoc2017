08bf76331c2e01d7272891410f2488a5d5e0df64
Ilya Kolodnik <ilya.kolodnik@gmail.com>
2018-01-28 21:04:53 +0300

    working day 14/2

--- day14/knots2.go
+++ day14/knots2.go
@@ -8,13 +8,67 @@ import (
 )
 
 var field [][]int
+var labels [][]int
+var queue [][]int
+var currentLabel int
 
 func main() {
 	field = make([][]int, 0, 0)
+	labels = make([][]int, 128)
+	queue = make([][]int, 0)
 	for i := 0; i < 128; i++ {
-		field = append(field, knot("flqrgnkx-", i))
+		field = append(field, knot("jzgqcdpd-", i))
+		labels[i] = make([]int, 128)
 	}
-	fmt.Println(field[0][0])
+	currentLabel = 1
+	pixelPos := 0
+	labels[0][0] = currentLabel
+	for pixelPos < 128*128 {
+		col := pixelPos % 128
+		row := pixelPos / 128
+		if field[row][col] == 1 && labels[row][col] == 0 {
+			labels[row][col] = currentLabel
+			coords := []int{row, col}
+			queue = append(queue, coords)
+			checkNeighbours()
+			currentLabel++
+		}
+		pixelPos++
+	}
+	fmt.Println(currentLabel - 1)
+}
+
+func checkNeighbours() {
+	for len(queue) > 0 {
+		// fmt.Println(queue)
+		var el []int
+		el, queue = queue[len(queue)-1], queue[:len(queue)-1]
+		neighbours := getNeighbours(el[0], el[1])
+		for _, el := range neighbours {
+			x, y := el[0], el[1]
+			if field[x][y] == 1 && labels[x][y] == 0 {
+				labels[x][y] = currentLabel
+				queue = append(queue, []int{x, y})
+			}
+		}
+	}
+}
+
+func getNeighbours(x int, y int) [][]int {
+	arr := make([][]int, 0)
+	if x > 0 {
+		arr = append(arr, []int{x - 1, y})
+	}
+	if x < 127 {
+		arr = append(arr, []int{x + 1, y})
+	}
+	if y > 0 {
+		arr = append(arr, []int{x, y - 1})
+	}
+	if y < 127 {
+		arr = append(arr, []int{x, y + 1})
+	}
+	return arr
 }
 
 func knot(str string, i int) []int {
