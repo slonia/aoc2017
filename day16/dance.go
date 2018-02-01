b69f78c124b458098d1ca93eba939df2adfcadd3
Ilya Kolodnik <ilya.kolodnik@gmail.com>
2018-02-01 21:02:56 +0300

    day 16/1

---
+++ day16/dance.go
@@ -0,0 +1,69 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+const sz int = 16
+
+var programs []rune
+
+func main() {
+	commands := []string{}
+	for i := 0; i < sz; i++ {
+		programs = append(programs, rune(i+97))
+	}
+	file, _ := os.Open("input.txt")
+	scanner := bufio.NewScanner(file)
+	scanner.Split(bufio.ScanLines)
+	for scanner.Scan() {
+		line := scanner.Text()
+		commands = append(commands, strings.Split(line, ",")...)
+	}
+	for _, command := range commands {
+		command, args := command[0], command[1:]
+		switch string(command) {
+		case "s":
+			spin(args)
+		case "x":
+			exchange(args)
+		case "p":
+			partner(args)
+		}
+	}
+	for i := 0; i < sz; i++ {
+		fmt.Printf("%c", programs[i])
+	}
+	fmt.Println()
+}
+
+func spin(command string) {
+	num, _ := strconv.Atoi(command)
+	programs = append(programs[(sz-num):], programs[:(sz-num)]...)
+}
+
+func exchange(command string) {
+	strs := strings.Split(command, "/")
+	a, _ := strconv.Atoi(strs[0])
+	b, _ := strconv.Atoi(strs[1])
+	programs[a], programs[b] = programs[b], programs[a]
+}
+
+func partner(command string) {
+	strs := strings.Split(command, "/")
+	a := indOf(strs[0])
+	b := indOf(strs[1])
+	programs[a], programs[b] = programs[b], programs[a]
+}
+func indOf(el string) int {
+	for i, str := range programs {
+		if str == rune(el[0]) {
+			return i
+		}
+	}
+	return -1
+}
