56d4e014e7f56211a017b7a2661a17e042056a1e
Ilya Kolodnik <ilya.kolodnik@gmail.com>
2018-02-11 22:27:26 +0300

    day 21/1 add struct

--- day21/fractal.go
+++ day21/fractal.go
@@ -4,22 +4,29 @@ import (
 	"bufio"
 	"os"
 	"strings"
+	"fmt"
 )
 
-var field []string = []string{".#.", "..#", "###"}
-var rules map[string]string
+type Rule struct {
+	from, to []string
+}
 
-const iterations int = 2
+var field []string = []string{".#.", "..#", "###"}
+var rules []Rule
+
+const iterations int = 1
 
 func main() {
-	rules = make(map[string]string)
+	rules = make([]Rule, 0)
 	file, _ := os.Open("input.txt")
 	scanner := bufio.NewScanner(file)
 	scanner.Split(bufio.ScanLines)
 	for scanner.Scan() {
 		line := scanner.Text()
 		split := strings.Split(line, " => ")
-		rules[split[0]] = split[1]
+		from := strings.Split(split[0], "/")
+		to := strings.Split(split[1], "/")
+		rules = append(rules, Rule{from, to})
 	}
 	for i := 0; i < iterations; i++ {
 		expand()
@@ -41,4 +48,26 @@ func expand() {
 		newStr := strings.Join(str, "")
 		newField = append(newField, newStr)
 	}
+	for i:=0; i < curLen/step; i++ {
+		square := make([]string, 0)
+		for j:=0; j < step; j++ {
+			square = append(square, field[j][(step*i):(step*(i+1))])
+		}
+		rule := findRule(square)
+	}
+}
+
+func findRule(square []string) Rule {
+	ln := len(square)
+	for _, rule := range rules {
+		for steps := 0; steps < 4; steps++ {
+			square = rotate(square)
+		}
+	}
+}
+
+func rotate(square []string) []string {
+	ln := len(square)
+	newSquare = make([]string, ln)
+	return square
 }
