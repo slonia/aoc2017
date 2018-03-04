419015c3fde0001150b1706479d97eb2014213d2
Ilya Kolodnik <ilya.kolodnik@gmail.com>
2018-03-04 14:29:24 +0300

    day 7/2 not working

--- day07/circus_balanced.go
+++ day07/circus_balanced.go
@@ -10,9 +10,11 @@ import (
 
 // This `person` struct type has `name` and `age` fields.
 type Node struct {
-	parent string
-	label  string
-	weight int
+	parent           string
+	label            string
+	weight           int
+	children         []string
+	calculatedWeight int
 }
 
 var nodes map[string]*Node
@@ -35,18 +37,69 @@ func main() {
 		weight, _ := strconv.Atoi(w_part[1 : len(w_part)-1])
 		_, ok := nodes[label]
 		if !ok {
-			nodes[label] = &Node{label: label, parent: "-"}
+			nodes[label] = &Node{label: label, parent: "-", children: make([]string, 0)}
 		}
 		nodes[label].weight = weight
 		if len(parts) > 1 {
 			setParent(strings.Split(parts[1], ","), label)
 		}
 	}
-	node := findParent(start)
-	fmt.Println(node.label)
+	rootLabel := findParent(start)
+	for _, node := range nodes {
+		parNode, ok := nodes[node.parent]
+		if ok {
+			parNode.children = append(parNode.children, node.label)
+		}
+	}
+	tree := []string{rootLabel}
+	for len(tree) > 0 {
+		root := nodes[tree[0]]
+		tree = tree[1:]
+		nodes[root.label].calculatedWeight = calcWeight(*root)
+		for _, el := range root.children {
+			tree = append(tree, el)
+		}
+	}
+	tree = []string{rootLabel}
+	wrongNode := nodes[rootLabel]
+	for len(tree) > 0 {
+		node := nodes[tree[0]]
+		tree = tree[1:]
+		// fmt.Println(node.label, isBalanced(*node))
+		if !isBalanced(*node) {
+			wrongNode = node
+			for _, child := range nodes[node.label].children {
+				tree = append(tree, child)
+			}
+		}
+	}
+	for _, label := range nodes[wrongNode.label].children {
+		child := nodes[label]
+		fmt.Println(label, child.weight, child.calculatedWeight)
+	}
 }
 
-func findParent(start string) Node {
+func isBalanced(node Node) bool {
+	ln := len(node.children)
+	for i := 0; i < ln-1; i++ {
+		child1 := nodes[node.children[i]]
+		child2 := nodes[node.children[i+1]]
+		if child1.calculatedWeight != child2.calculatedWeight {
+			return false
+		}
+	}
+	return true
+}
+
+func calcWeight(node Node) int {
+	weight := node.weight
+	for _, label := range node.children {
+		child := nodes[label]
+		weight += child.weight
+	}
+	return weight
+}
+func findParent(start string) string {
 	node := nodes[start]
 	for {
 		if node.parent != "-" {
@@ -55,7 +108,7 @@ func findParent(start string) Node {
 			break
 		}
 	}
-	return *node
+	return node.label
 }
 
 func setParent(parts []string, parent string) {
@@ -65,7 +118,7 @@ func setParent(parts []string, parent string) {
 		if ok {
 			node.parent = parent
 		} else {
-			nodes[label] = &Node{label: label, parent: parent}
+			nodes[label] = &Node{label: label, parent: parent, children: make([]string, 0)}
 		}
 	}
 }
